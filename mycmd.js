afb34a3191933380bba2f1cba9c5c5d9442be7a2
Steve Heckt <home@u2me.com>
2015-02-11 21:31:53 -0800

    App structure using simplified Liftoff, arg parsing using Minimist

--- mycmd.js
+++ mycmd.js
@@ -1,3 +1,21 @@
 #!/usr/bin/env node
 
-console.log("hello");
+const Liftoff = require('liftoff');
+
+const MyCmd = new Liftoff({
+    name: 'mycmd',
+    config: '.mycmd'
+});
+
+const mycmd_logic = function(env) {
+    console.log('my environment is:', env);
+    console.log('my cli options are:', argv);
+    console.log('my liftoff config is:', this);
+};
+
+const argv = require('minimist')(process.argv.slice(2));
+
+MyCmd.launch(
+    {},
+    mycmd_logic
+);
